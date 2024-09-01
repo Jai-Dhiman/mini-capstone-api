f4193a4277ad1c252d3c8e855cd2b9c3ea7c9849
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-01 15:43:50 -0700

    added update and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,23 @@ class ProductsController < ApplicationController
       description: params[:description],
       image_url: params[:image_url],
     )
+    @product.saves
+    render template: "products/show"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.image_url = params[:image_url]
     @product.save
     render template: "products/show"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "product has been removed" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   post "/products" => "products#create"
 
+  patch "/products/:id" => "products#update"
+
+  delete "/products/:id" => "products#destroy"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
