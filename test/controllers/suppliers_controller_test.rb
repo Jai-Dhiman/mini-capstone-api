395cf073aa8396fdf482900686d74b77e6f6d273
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-08 15:31:39 -0700

    added supplier table and controller

---
+++ test/controllers/suppliers_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class SuppliersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get suppliers_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get suppliers_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get suppliers_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get suppliers_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get suppliers_destroy_url
+    assert_response :success
+  end
+end
