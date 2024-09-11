c5bf6b9cdeaeceb648df26b14e693d21ebb7c49d
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-10 19:29:38 -0700

    test fixes

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,8 +15,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "", price: 0, description: "", image_url: "" }
-      post "/products.json", params: { name: "1", price: 1, description: "hello", image_url: "hjvsdf.jpg" }
+      post "/products.json", params: { name: "", price: 0, description: ""}
+      post "/products.json", params: { name: "1", price: 1, description: "hello" }
       
       assert_response 200
     end
@@ -27,7 +27,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["id", "name", "price", "description", "image_url", "is_discounted?", "tax", "total"], data.keys
+    assert_equal ["id", "name", "price", "description", "is_discounted?", "tax", "total"], data.keys
   end
 
   test "update" do
