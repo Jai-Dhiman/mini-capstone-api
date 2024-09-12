930b9b4ca7025aa1e7d90130913a2f23cfbbf417
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-11 19:15:04 -0700

    added to orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,28 +7,24 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/products" => "products#index"
-
   get "/products/:id" => "products#show"
-
   post "/products" => "products#create"
-
   patch "/products/:id" => "products#update"
-
   delete "/products/:id" => "products#destroy"
 
   get "/suppliers" => "suppliers#index"
-
   get "/suppliers/:id" => "suppliers#show"
-
   post "/suppliers" => "suppliers#create"
-
   patch "/suppliers/:id" => "suppliers#update"
-
   delete "/suppliers/:id" => "suppliers#destroy"
 
   post "/users" => "users#create"
 
   post "/sessions" => "sessions#create"
+
+  get "/orders" => "orders#index"
+  get "/orders/:id" => "orders#show"
+  post "/orders" => "orders#create"
   # Defines the root path route ("/")
   # root "posts#index"
 end
