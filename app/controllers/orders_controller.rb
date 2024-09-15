43b13d0ab3ece98160e6294c8a6d39b70b129556
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-15 08:58:21 -0700

    update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,12 +11,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+    
     @orders = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
-
     if @orders.save
       render :show
     else
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
-
-  def subtotal
-    product.price * quantity
-  end
-
-  def tax
-    subtotal * 0.09
-  end
-
-  def total
-    subtotal + (subtotal * 0.09)
-  end
 end
