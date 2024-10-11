cab37e030b3b6e68be9b525e0d0b9ad25bf20a2f
JaiDhiman <jaidhiman2000@gmail.com>
2024-10-10 19:02:22 -0700

    orders starter

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,22 @@
+# app/controllers/orders_controller.rb
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def create
     @order = current_user.orders.new
-    @order.carted_products = current_user.carted_products.in_cart
+    @carted_products = current_user.carted_products.where(status: "in_cart")
     
-    if @order.carted_products.any? && @order.complete_order && @order.save
-      render json: @order, include: { carted_products: { include: :product } }
+    if @carted_products.any? && @order.save
+      @carted_products.update_all(status: "purchased", order_id: @order.id)
+      @order.calculate_totals
+      render json: @order, include: { carted_products: { include: :product } }, status: :created
     else
       render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+    render json: @order, include: { carted_products: { include: :product } }
   end
 end
